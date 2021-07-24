bf75fb9cdd09229f6a3d58c9c81181d3c509278c
leo812leo <leo_812leo@hotmail.com.tw>
2021-07-24 13:08:05 +0800

    views edit and new_modify

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/expense/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/expense/edit', (req, res) => {
+  res.render('edit')
+})
+
 app.listen(PORT, () => {
   console.log(`APP is running on port http://localhost:${PORT}`)
 })
