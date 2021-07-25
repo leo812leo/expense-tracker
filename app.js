58c26e12a1a0bf80a51f1c5e477203e431dadfbc
leo812leo <leo_812leo@hotmail.com.tw>
2021-07-25 17:55:32 +0800

    add moment modules & set routes with mongoose data

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-//import 
+//import modules
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const PORT = 3000
-
+const moment = require('moment')
 require('./config/mongoose')
+//Data
+const Record = require('./models/expense')
+const Category = require('./models/category')
 
+
+
+//seeting
+const PORT = 3000
 app = express()
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-//seeting
+
+
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // asynchronous
+  const categoryToClass = {}
+  Promise.all([Record.find().lean(), Category.find().lean()])
+    .then(results => {
+      const [records, categories] = results
+      //make category2class dictionary
+      categories.forEach(category => {
+        categoryToClass[category['name']] = category.iconClass
+      })
+      //add iconclass to records
+      records.forEach(record => {
+        record.iconClass = categoryToClass[record.category]
+        record.date = moment(record.date).format('YYYY-MM-DD')
+      })
+      console.log(records)
+      //render
+      res.render('index', { records })
+    })
+    .catch(err => console.log(err))
 })
+
+
+
+
 
 app.get('/expense/new', (req, res) => {
   res.render('new')
