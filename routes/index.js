ed853af67a570b930a1fb4e3c8228ab0cf6e6375
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-10 23:59:54 +0800

    feat: add user authentication in routers

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const users = require('./modules/users')
+// import function
+const { authenticator } = require('../middleware/auth')
 // 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
-router.use('/expense', expense)
+
+router.use('/expense', authenticator, expense)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
