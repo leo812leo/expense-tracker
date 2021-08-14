236a8d0a1dca3a5157e65fd5c5891e7d27afe972
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-14 21:43:13 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-const usePassport = require('./config/passport')
+const flash = require('connect-flash')
+
 /* import User define modules */
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 require('./config/mongoose')
-//Data
-const Record = require('./models/expense')
-const Category = require('./models/category')
 
 // seeting
 const app = express()
@@ -31,11 +30,15 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 // passport
 usePassport(app)
+// flash
+app.use(flash())
 // 新增middleware
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 // 將 request 導入路由器
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -17,6 +17,7 @@ router.post('/login', passport.authenticate('local', {
 /* logout */
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
@@ -28,9 +29,26 @@ router.get('/register', (req, res) => {
 // register post
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
       console.log('User already exists.')
+      req.flash('warning_msg', 'User already exists')
       return res.render('register', {
         name, email, password, confirmPassword
       })
